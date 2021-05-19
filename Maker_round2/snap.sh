96a63f9a627dd9fcad2d06cb877adb70aaf3e3fc
Anjana_CK <43054305+akaraw@users.noreply.github.com>
2021-05-20 09:51:30 +1000

    Update snap.sh

--- Maker_round2/snap.sh
+++ Maker_round2/snap.sh
@@ -7,7 +7,7 @@ maker2zff -x 0.25 -l 50 -d ../../round1_${gen}.maker.output/round1_${gen}_master
 rename genome ${gen}_rnd1.zff.length50_aed0.25 *
 fathom ${gen}_rnd1.zff.length50_aed0.25.ann ${gen}_rnd1.zff.length50_aed0.25.dna -gene-stats > gene-stats.log 2>&1
 fathom ${gen}_rnd1.zff.length50_aed0.25.ann ${gen}_rnd1.zff.length50_aed0.25.dna -validate > validate.log 2>&1
-fathom $[gen}_rnd1.zff.length50_aed0.25.ann ${gen}_rnd1.zff.length50_aed0.25.dna -categorize 1000 > categorize.log 2>&1
+fathom ${gen}_rnd1.zff.length50_aed0.25.ann ${gen}_rnd1.zff.length50_aed0.25.dna -categorize 1000 > categorize.log 2>&1
 fathom uni.ann uni.dna -export 1000 -plus > uni-plus.log 2>&1
 # create the training parameters
 mkdir params
