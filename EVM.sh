79037633343af43bf8ffcc2616fe69490905c35b
Anjana_CK <43054305+akaraw@users.noreply.github.com>
2021-06-30 17:24:07 +1000

    Update EVM.sh

--- EVM.sh
+++ EVM.sh
@@ -24,7 +24,7 @@ Launch_PASA_pipeline.pl \
 create_weights_file.pl -T evm/pasa/annot.sqlite3.pasa_assemblies.gff3 -P public.gff -A rnd_1_2_3_merged.gff > weights.txt
 
 partition_EVM_inputs.pl --genome BS_postgapfiller.fa --gene_predictions rnd_1_2_3_merged.gff --transcript_alignments annot.sqlite3.pasa_assemblies.gff3 \
---protein_alignments public.gff --segmentSize 1000000 --overlapSize 10000 --partition_listing partitions_list.out
+--protein_alignments pubgemo.gff --segmentSize 1000000 --overlapSize 10000 --partition_listing partitions_list.out
 
 write_EVM_commands.pl --genome BS_postgapfiller.fa --weights `pwd`/weights.txt --gene_predictions rnd_1_2_3_merged.gff --protein_alignments pubgemo.gff \
 --transcript_alignments annot.sqlite3.pasa_assemblies.gff3 --output_file_name evm.out  --partition partitions_list.out >  commands.list
