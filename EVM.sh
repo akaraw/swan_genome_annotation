8c41d6e187f33b29460e30d860ce7786d8a6efac
Anjana_CK <43054305+akaraw@users.noreply.github.com>
2021-06-30 17:11:31 +1000

    Update EVM.sh

--- EVM.sh
+++ EVM.sh
@@ -21,8 +21,7 @@ mamba install -c bioconda pasa
 Launch_PASA_pipeline.pl \
 --CPU 16 -C -R --ALIGNER gmap,blat -g bs.fa -t bs_transcripts.fa -c alignAssembly.conf -u bs_transcripts.fa
 
-create_weights_file.pl -T \
-evm/pasa/annot.sqlite3.pasa_assemblies.gff3 -P public.gff -A rnd_1_2_3_merged.gff > weights.txt
+create_weights_file.pl -T evm/pasa/annot.sqlite3.pasa_assemblies.gff3 -P public.gff -A rnd_1_2_3_merged.gff > weights.txt
 
 partition_EVM_inputs.pl --genome BS_postgapfiller.fa --gene_predictions rnd_1_2_3_merged.gff --transcript_alignments annot.sqlite3.pasa_assemblies.gff3 \
 --protein_alignments public.gff --segmentSize 1000000 --overlapSize 10000 --partition_listing partitions_list.out
