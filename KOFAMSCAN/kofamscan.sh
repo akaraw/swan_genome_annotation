55c4e5d7212214d7e11e28f14c091e3c5e179d40
Anjana_CK <43054305+akaraw@users.noreply.github.com>
2021-07-12 18:28:36 +1000

    Update kofamscan.sh

--- KOFAMSCAN/kofamscan.sh
+++ KOFAMSCAN/kofamscan.sh
@@ -7,3 +7,5 @@ wget ftp://ftp.genome.jp/pub/db/kofam/profiles.tar.gz
 gunzip ko_list.gz
 tar xf profiles.tar.gz
 
+exec_annotation -c config.yml -E 0.00001 --tmp-dir ${TMPDIR} -f mapper -o bs.ens.txt bs.ens.faa
+
