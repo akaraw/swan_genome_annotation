96b5cae04434c6c66b7f4aa76bb2e1d8a4b2ae16
Anjana_CK <ackarawita@yahoo.com>
2021-08-19 20:27:01 +1000

    Update flnc_to_genome.sh

--- TAMA_code/flnc_to_genome.sh
+++ TAMA_code/flnc_to_genome.sh
@@ -1,3 +1,5 @@
+#Dependecies TAMA, bedtools, samtools, python2, seqkit, blast+
+
 #Convert flnc.bam file to fasta
 samtools fasta -@ 24 $bam > flnc.$gen.fa
 
@@ -17,4 +19,26 @@ for i in ms_*.sam; do base=$(basename $i ".sam"); python2 tama_collapse.py -a 10
 #Merging sperate collapse results
 python2 python2 tama/tama/tama_merge.py -f file_run1.txt -p firstrun
 python2 python2 tama/tama/tama_merge.py -f file_run2.txt -p secondrun
-python2 python2 tama/tama/tama_merge.py -f file_merge.txt -p final
+python2 python2 tama/tama/tama_merge.py -f file_final.txt -p final_${gen}
+
+#Get fasta and ORF with seqkit and bedtools
+bedtools getfasta -name -split -s -fi ${gen}.fasta -bed final_${gen}.bed -fo ${gen}_iso.fasta
+python2 tama/tama/tama_go/orf_nmd_predictions/tama_orf_seeker.py -f ${gen}_iso.fasta -o ${gen}_iso.aa
+seqkit seq -m 20 ${gen}_iso.aa > ${gen}_iso.20.aa
+seqkit split ms_iso.20.aa -p 100
+
+#Export function
+function tama_blast() { n=$(basename "$1" ".fa"); blastp -evalue 1e-10 -db Anas_platyrhynchos.fa -num_threads 1 -max_hsps 1 -out ${n}.txt -query $1; }
+export -f tama_blast
+
+#Run blastp in parallel
+find ${gen}_iso.20.aa.split/ -type f -name "*aa" | parallel -j 24 tama {}
+cat ${gen}_iso.20.part_*txt > ${gen}_iso.20.txt
+
+#Parsing blastp results
+python2 tama/tama/tama_go/orf_nmd_predictions/tama_orf_blastp_parser.py -b ${gen}_iso.20.txt -o ${gen}_blastp -f ensembl
+
+
+
+
+
